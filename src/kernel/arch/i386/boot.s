0cccf272279edcd8f4aa86187f03a44ccac34ac9
Cormac O'Brien <i.am.cormac.obrien@gmail.com>
2014-10-05 15:52:07 -0500

    Added src/ and doc/.

---
+++ src/kernel/arch/i386/boot.s
@@ -0,0 +1,29 @@
+MB_ALIGN    equ 1<<0
+MB_MEMINFO  equ 1<<1
+MB_FLAGS    equ MB_ALIGN | MB_MEMINFO
+MB_MAGIC    equ 0x1badb002
+MB_CHECKSUM equ -(MB_FLAGS + MB_MAGIC)
+
+section .multiboot
+align 4
+    dd MB_MAGIC
+    dd MB_FLAGS
+    dd MB_CHECKSUM
+
+section .boot_stack
+stack_bottom:
+times 16384 db 0 ; allocate 16 KiB
+stack_top:
+
+section .text
+global _start
+_start:
+    mov esp, stack_top
+
+    extern kernel_main
+    call kernel_main
+    
+    cli
+.hang:
+    hlt
+    jmp .hang
